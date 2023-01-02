52eaf58c16749aecfb4c7b79ff49e89bab7b3160
Holger Ballweg <hb@uiae.de>
2023-01-02 22:00:19 +0000

    feat: look for url as well as text

--- embed.js
+++ embed.js
@@ -1,7 +1,8 @@
 // from https://unpkg.com/@strudel.cycles/embed@0.2.0/embed.js, 
 // changes: 
 // * added timeout
-// * set width to standard timeline width on hachyderm
+// * set width to 100%
+// * use url attribute instead of inline code if url is supplied
 
 class Strudel extends HTMLElement {
   constructor() {
@@ -9,18 +10,19 @@ class Strudel extends HTMLElement {
   }
   connectedCallback() {
     setTimeout(() => {
+      const url = this.getAttribute('url')
       const code = (this.innerHTML + '').replace('<!--', '').replace('-->', '').trim();
       const iframe = document.createElement('iframe');
-      const src = `https://strudel.tidalcycles.org/#${encodeURIComponent(btoa(code))}`;
-      // const src = `http://localhost:3000/#${encodeURIComponent(btoa(code))}`;
+      const src = url.startsWith('https://strudel.tidalcycles.org')
+        ? url 
+        : `https://strudel.tidalcycles.org/#${encodeURIComponent(btoa(code))}`;
       iframe.setAttribute('src', src);
       iframe.setAttribute('width', '100%');
       iframe.setAttribute('height', '300');
       this.appendChild(iframe);
-
     },
     // need to set some timeout to not mess up scrolling in Mastodon 
-    150);
+    500);
   }
 }
 customElements.define('strudel-repl', Strudel);
--- topfenstrudel.js
+++ topfenstrudel.js
@@ -1,8 +1,23 @@
 // find all content texts
 ready('div.status__content__text', el =>  
   { 
-    // look for the special strudel hashtag
-    if (el.innerText.includes('// #strudel')) {
+    const strudelLink = el.querySelector('a.status-link[href^="https://strudel.tidalcycles.org"]')
+    // look for strudel share link
+    if (strudelLink) {
+      // make a new strudel repl
+      const url = strudelLink.getAttribute('href')
+      const repl = document.createElement('strudel-repl')
+      repl.setAttribute('url', url)
+      // pack it in a div so react (?) doesn't throw an error 
+      // as the repl is a restricted element and it tries to read an attribute from it
+      const div = document.createElement('div')
+      div.appendChild(repl)
+
+      // replace post text with repl
+      el.appendChild(div)
+    }
+    // OR look for the special strudel hashtag
+    else if (el.innerText.includes('// #strudel')) {
       // make a new strudel repl
       const repl = document.createElement('strudel-repl')
       repl.innerHTML = `\n<!-- \n${el.innerText} \n --> \n`
